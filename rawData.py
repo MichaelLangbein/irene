b934476ef7118b64d17398af249c77baaf730908
Michael Langbein <michael.langbein@lfu.bayern.de>
2018-11-16 15:48:45 +0100

    httpDownload funzt

--- rawData.py
+++ rawData.py
@@ -6,14 +6,42 @@
 # Aktuelle Daten (Radar nur binär): https://opendata.dwd.de/weather/nwp/
 # Historische Daten (aber keine Modellvorhersagen): ftp://ftp-cdc.dwd.de/pub/CDC/
 
+import os
+import urllib.request
+from ftplib import FTP
 
-urlRadolan = "ftp://ftp-cdc.dwd.de/pub/CDC/grids_germany/hourly/radolan/recent/asc/" # aktuelle radardaten in ascii format
-urlCD2 = "https://opendata.dwd.de/weather/nwp/cosmo-d2/grib/" #aktuelle modelldaten in grib2 format
-rawDataDir = "rawData"
 
 
-def downloadFile(url, fileName, targetDir):
-    pass
+thisDir = os.path.dirname(os.path.abspath(__file__))
+rawDataDir = thisDir + "/rawData/"
+dwdFtpServer = "ftp://ftp-cdc.dwd.de/"
+radolanPath = "pub/CDC/grids_germany/hourly/radolan/recent/asc/"
+dwdODServer = "https://opendata.dwd.de/"
+cosmoD2Path = "weather/nwp/cosmo-d2/grib/"
+
+
+
+def httpDownloadFile(serverName, path, fileName, targetDir):
+    fullUrl = serverName + path + fileName
+    fullFile = targetDir + fileName
+    print("Now attempting connection to {}".format(fullUrl))
+    with urllib.request.urlopen(fullUrl) as response:
+        with open(fullFile, 'wb') as fileHandle:
+            print("Now saving data in {}".format(fullFile))
+            data = response.read()  # a bytes-object
+            fileHandle.write(data)
+
+
+def ftpDownloadFile(serverName, path, fileName, targetDir):
+    fullFile = targetDir + fileName
+    print("Now attempting connection to {}".format(serverName))
+    with FTP(serverName) as ftp:
+        with open(fullFile, 'wb') as fileHandle:
+            ftp.login()
+            ftp.cwd(path)
+            print("Now saving data in {}".format(fullFile))
+            tfp.retrbinary(fileName, fileHandle.write)
+        
 
 
 def downloadRadar(date):
@@ -30,3 +58,9 @@ def getModelData(fromTime, toTime, bbox, parameters):
 
 def getRadarData(fromTime, toTime, bbox, parameters):
     pass
+
+
+
+if __name__ == "__main__":
+    httpDownloadFile(dwdODServer, cosmoD2Path + "00/clc/", "cosmo-d2_germany_regular-lat-lon_model-level_2018111600_001_52_CLC.grib2.bz2", rawDataDir) 
+    ftpDownloadFile(dwdFtpServer, radolanPath, "RW-20180101.tar.gz", rawDataDir)
