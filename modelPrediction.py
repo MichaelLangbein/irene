e17867b5a7690d451edd77714ab28d97876397b1
MichaelLangbein <michael_langbein@hotmail.de>
2019-06-24 09:28:45 +0200

    some refactoring of plotting

--- modelPrediction.py
+++ modelPrediction.py
@@ -4,14 +4,16 @@ import datetime as dt
 import radarData2 as rd
 import numpy as np
 import plotting as pl
+import matplotlib.pyplot as plt
 
 
-thisDir = os.path.dirname(os.path.abspath(__file__))
+thisDir = os.path.abspath('')
 tfDataDir = thisDir + "/tfData/"
 modelName = "latestRadPredModel.h5"
 
 
 model = k.models.load_model(tfDataDir + modelName)
+print(model.summary())
 dataIn, dataOut = rd.loadTfData("validation_2016.h5", int(5 * 60/5), 10)
 prediction = model.predict(dataIn)
 
@@ -22,7 +24,9 @@ pl.movie(maxActInpt[:, :, :, 0], ["maximal activationImage for conv2 channel 0 "
 maxActInpt = pl.getMaximallyActivatingImage(model, "conv3", 0, dataIn[0].shape)
 pl.movie(maxActInpt[:, :, :, 0], ["maximal activationImage for conv3 channel 0 "], 100)
 
-pl.plotActivations(model, dataIn[6], "conv3")
+fig, axArr = pl.plotActivations(model, dataIn[6], "conv3")
+axArr = np.reshape(axArr, (4, 4))
+plt.show()
 
 
 
